cd37e0447e8d5e6825286a094722c38fdc4e5dd1
jannydream0826 <fation.qama.god@gmail.com>
2023-08-15 00:22:05 +0200

    feat: integrate entities to app.module.ts

---
+++ src/typeorm/index.ts
@@ -0,0 +1,12 @@
+import { Fighter } from "./fighter.entity";
+import { Fight } from "./fight.entity";
+import { Event } from "./event.entity";
+import { Ranking } from "./ranking.entity";
+
+const entities = [Fighter, Fight, Event, Ranking];
+
+export {Fighter};
+export {Fight};
+export {Event};
+export {Ranking};
+export default entities;
