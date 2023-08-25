5b0bcddef3c9ea49e73921e7b0585d4495d54870
jannydream0826 <fation.qama.god@gmail.com>
2023-08-25 21:54:52 +0200

    fix: update data-source.ts with .env

--- db/data-source.ts
+++ db/data-source.ts
@@ -1,12 +1,12 @@
 import { DataSource, DataSourceOptions } from "typeorm";
 
 export const dataSourceOptions: DataSourceOptions ={
-    type: 'postgres',
-    host: 'db',
-    port: 5432,
-    username: 'postgres',
-    password: 'postgres',
-    database: 'postgres',
+    type: process.env.DB_TYPE as any,
+    host: process.env.PG_HOST,
+    port: parseInt(process.env.PG_PORT),
+    username: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DB,
     entities: ['dist/**/*.entity.js'],
     migrations: ['dist/db/migrations/*.js'],
     synchronize: true,
